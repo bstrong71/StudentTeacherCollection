184365b6eacfc6f56e2b64a85e5298efc2787f2f
Bernard Strong <bstrong71@gmail.com>
2017-09-20 16:44:03 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Student> allStudents = new ArrayList<>();
+
+        Student newStudent = new Student("Adam", "Baker", 100, 1);
+        allStudents.add(newStudent);
+        Student newStudent = new Student("Eve", "Jones", 101, 1);
+        allStudents.add(newStudent);
+        Student newStudent = new Student("Joe", "Blake", 102, 1);
+        allStudents.add(newStudent);
+        Student newStudent = new Student("John", "Smith", 103, 1);
+        allStudents.add(newStudent);
+        Student newStudent = new Student("Sara", "Lipscomb", 104, 1);
+        allStudents.add(newStudent);
+        Student newStudent = new Student("Amy", "Lawrence", 105, 1);
+        allStudents.add(newStudent);
+        Student newStudent = new Student("Tony", "Brown", 106, 1);
+        allStudents.add(newStudent);
+        Student newStudent = new Student("Jeff", "Taggert", 107, 1);
+        allStudents.add(newStudent);
+        Student newStudent = new Student("Julie", "Johnson", 108, 1);
+        allStudents.add(newStudent);
+        Student newStudent = new Student("Michelle", "Allen", 109, 1);
+        allStudents.add(newStudent);
+        Student newStudent = new Student("Willy", "Clinton", 110, 1);
+        allStudents.add(newStudent);
+        Student newStudent = new Student("George", "Bush", 111, 1);
+        allStudents.add(newStudent);
+        Student newStudent = new Student("Pam", "Dawber", 112, 1);
+        allStudents.add(newStudent);
+        Student newStudent = new Student("Lisa", "Stephens", 113, 1);
+        allStudents.add(newStudent);
+        Student newStudent = new Student("James", "Walters", 114, 1);
+        allStudents.add(newStudent);
+
+        
+    }
+}
---
+++ src/Teacher.java
@@ -0,0 +1,74 @@
+public class Teacher {
+
+    String firstName;
+    String lastName;
+    int id;
+    int grade;
+
+    public Teacher() {
+    }
+
+    public Teacher(String firstName, String lastName, int id, int grade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.grade = grade;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Teacher teacher = (Teacher) o;
+
+        return id == teacher.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                ", grade=" + grade +
+                '}';
+    }
+}
